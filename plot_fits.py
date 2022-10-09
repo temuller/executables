c6bb0c1f671aba1e0224b5b274d64d181bfbfd54
temuller <t.e.muller-bravo@ice.csic.es>
2022-10-09 07:31:23 +0200

    add colour bar and option for data on [1]

--- plot_fits.py
+++ plot_fits.py
@@ -8,11 +8,14 @@ def plot_fits(input_file):
 
     img = fits.open(input_file)
     data = img[0].data
+    if data is None:
+        data = img[1].data
 
     fig, ax = plt.subplots()
     m, s = np.nanmean(data), np.nanstd(data)
     im = ax.imshow(data, interpolation='nearest', origin='lower',
                    cmap='gray', vmin=m-s, vmax=m+s)
+    fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
     plt.show()
 
 def main():
